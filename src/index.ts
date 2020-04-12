baeeac849d5d60d94b95686fe6224465bb13a7bd
Daniel Fischer <daniel@d-fischer.dev>
2020-04-12 15:53:57 +0200

    use my own stack and add native ESM build

--- src/index.ts
+++ src/index.ts
@@ -26,81 +26,77 @@ type EventHandler<Args extends any[]> = (...args: Args) => void;
 type EventBinder<Args extends any[]> = (event: EventHandler<Args>) => Listener;
 
 export class EventEmitter {
-  private eventListeners: Map<Function, Function[]>;
-  
-  constructor() {
-    this.eventListeners = new Map();
-  }
-  
-  on<Args extends any[]>(event: EventBinder<Args>, listener: EventHandler<Args>) {
-    if(!this.eventListeners.has(event)) {
-      this.eventListeners.set(event, [ listener ]);
-    } else {
-      this.eventListeners.get(event).push(listener);
-    }
-    
-    return new Listener(this, event, listener);
-  }
-  
-  addListener<Args extends any[]>(event: EventBinder<Args>, listener: EventHandler<Args>) {
-    return this.on(event, listener);
-  }
-  
-  removeListener();
-  removeListener(id: Listener);
-  removeListener(event: Function, listener?: Function);
-  
-  removeListener() {
-    if(arguments.length == 0) {
-      this.eventListeners.clear();
-    } else if(arguments.length == 1 && typeof arguments[0] == 'object') {
-      let id = arguments[0];
-      this.removeListener(id.event, id.listener);
-    } else if(arguments.length >= 1) {
-      let event = <Function>arguments[0];
-      let listener = <Function>arguments[1];
-      
-      if(this.eventListeners.has(event)) {
-        var listeners = this.eventListeners.get(event);
-        var idx;
-        while(!listener || (idx = listeners.indexOf(listener)) != -1) {
-          listeners.splice(idx, 1);
-        }
-      }
-    }
-  }
-  
-  /**
-   * Emit event. Calls all bound listeners with args.
-   */
-  protected emit<Args extends any[]>(event: EventBinder<Args>, ...args: Args) {
-    if(this.eventListeners.has(event)) {
-      for(var listener of this.eventListeners.get(event)) {
-        listener(...args);
-      }
-    }
-  }
-  
-  /**
-   * @typeparam T The event handler signature.
-   */
-  registerEvent<T extends EventHandler<any[]>>() {
-    const eventBinder = (handler: T): Listener => {
-      return this.addListener(eventBinder, handler);
-    };
-    
-    return eventBinder;
-  }
+	private eventListeners: Map<Function, Function[]>;
+
+	constructor() {
+		this.eventListeners = new Map();
+	}
+
+	on<Args extends any[]>(event: EventBinder<Args>, listener: EventHandler<Args>) {
+		if (!this.eventListeners.has(event)) {
+			this.eventListeners.set(event, [listener]);
+		} else {
+			this.eventListeners.get(event)!.push(listener);
+		}
+
+		return new Listener(this, event, listener);
+	}
+
+	addListener<Args extends any[]>(event: EventBinder<Args>, listener: EventHandler<Args>) {
+		return this.on(event, listener);
+	}
+
+	removeListener(): void;
+	removeListener(id: Listener): void;
+	removeListener(event: Function, listener?: Function): void;
+
+	removeListener() {
+		if (arguments.length == 0) {
+			this.eventListeners.clear();
+		} else if (arguments.length == 1 && typeof arguments[0] == 'object') {
+			let id = arguments[0];
+			this.removeListener(id.event, id.listener);
+		} else if (arguments.length >= 1) {
+			let event = <Function>arguments[0];
+			let listener = <Function>arguments[1];
+
+			if (this.eventListeners.has(event)) {
+				const listeners = this.eventListeners.get(event)!;
+				let idx = 0;
+				while (!listener || (idx = listeners.indexOf(listener)) != -1) {
+					listeners.splice(idx, 1);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Emit event. Calls all bound listeners with args.
+	 */
+	protected emit<Args extends any[]>(event: EventBinder<Args>, ...args: Args) {
+		if (this.eventListeners.has(event)) {
+			for (const listener of this.eventListeners.get(event)!) {
+				listener(...args);
+			}
+		}
+	}
+
+	/**
+	 * @typeparam T The event handler signature.
+	 */
+	registerEvent<T extends EventHandler<any[]>>() {
+		const eventBinder = (handler: T): Listener => {
+			return this.addListener(eventBinder, handler);
+		};
+
+		return eventBinder;
+	}
 }
 
 export class Listener {
-  constructor(public owner: EventEmitter,
-    public event: Function,
-    public listener: Function) {
-    
-  }
-  
-  unbind() {
-    this.owner.removeListener(this);
-  }
+	constructor(public owner: EventEmitter, public event: Function, public listener: Function) {}
+
+	unbind() {
+		this.owner.removeListener(this);
+	}
 }
