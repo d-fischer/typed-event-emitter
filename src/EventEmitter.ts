054a78f7fe7586628f4bda90f213ac81e07c2aa7
Daniel Fischer <daniel@d-fischer.dev>
2020-09-21 22:13:38 +0200

    update packages

--- src/EventEmitter.ts
+++ src/EventEmitter.ts
@@ -4,17 +4,17 @@ export type EventHandler<Args extends any[]> = (...args: Args) => void;
 export type EventBinder<Args extends any[]> = (event: EventHandler<Args>) => Listener;
 
 export class EventEmitter {
-	private eventListeners: Map<Function, Function[]>;
+	private _eventListeners: Map<Function, Function[]>;
 
 	constructor() {
-		this.eventListeners = new Map();
+		this._eventListeners = new Map();
 	}
 
 	on<Args extends any[]>(event: EventBinder<Args>, listener: EventHandler<Args>) {
-		if (this.eventListeners.has(event)) {
-			this.eventListeners.get(event)!.push(listener);
+		if (this._eventListeners.has(event)) {
+			this._eventListeners.get(event)!.push(listener);
 		} else {
-			this.eventListeners.set(event, [listener]);
+			this._eventListeners.set(event, [listener]);
 		}
 
 		return new Listener(this, event, listener);
@@ -30,22 +30,22 @@ export class EventEmitter {
 
 	removeListener(idOrEvent?: Listener | Function, listener?: Function) {
 		if (!idOrEvent) {
-			this.eventListeners.clear();
+			this._eventListeners.clear();
 		} else if (typeof idOrEvent === 'object') {
 			const id = idOrEvent;
 			this.removeListener(id.event, id.listener);
 		} else {
 			const event = idOrEvent;
 
-			if (this.eventListeners.has(event)) {
+			if (this._eventListeners.has(event)) {
 				if (listener) {
-					const listeners = this.eventListeners.get(event)!;
+					const listeners = this._eventListeners.get(event)!;
 					let idx = 0;
 					while ((idx = listeners.indexOf(listener)) !== -1) {
 						listeners.splice(idx, 1);
 					}
 				} else {
-					this.eventListeners.delete(event);
+					this._eventListeners.delete(event);
 				}
 			}
 		}
@@ -64,8 +64,8 @@ export class EventEmitter {
 	 * Emit event. Calls all bound listeners with args.
 	 */
 	protected emit<Args extends any[]>(event: EventBinder<Args>, ...args: Args) {
-		if (this.eventListeners.has(event)) {
-			for (const listener of this.eventListeners.get(event)!) {
+		if (this._eventListeners.has(event)) {
+			for (const listener of this._eventListeners.get(event)!) {
 				listener(...args);
 			}
 		}
